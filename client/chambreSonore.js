8487e7ac826db077f3e0a3379837671da511858b
Nicolas Joseph <nicolas@linkurio.us>
2015-02-22 21:21:43 +0100

    Finish some of the design material

--- client/chambreSonore.js
+++ client/chambreSonore.js
@@ -1,48 +1,39 @@
 var
-  player,
-  done = false;
+  player;
 
-HTTP.get('https://www.googleapis.com/youtube/v3/playlistItems', {
-  params: {
-    playlistId: 'UU5qblOHgtY5kWb0MnhCYmJA',
-    order: 'date',
-    part: 'snippet',
-    key: 'AIzaSyC9heFBAikx36BavK0fxNHr3k4Hz5ttgII'
-  }
-}, function(error, result){
-  if (error){
-    console.error(error);
-  } else {
-    console.log(result.data);
-    Session.set('videos', result.data);
-  }
-});
+onYouTubeIframeAPIReady = function() {
+  console.log('entering callback from youtube')
+  HTTP.get('https://www.googleapis.com/youtube/v3/playlistItems', {
+    params: {
+      playlistId: 'UU5qblOHgtY5kWb0MnhCYmJA',
+      order: 'date',
+      part: 'snippet',
+      key: 'AIzaSyC9heFBAikx36BavK0fxNHr3k4Hz5ttgII'
+    }
+  }, function(error, result){
+    if (error){
+      console.error(error);
+    } else {
+      console.log(result.data);
+
+      player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: result.data.items[0].snippet.resourceId.videoId
+      });
+
+      Session.set('videos', result.data);
+    }
+  });
+}
 
 Template.body.helpers({
-  firstVideo: function(){
-    var videos = Session.get('videos');
-    if (videos && videos.items && videos.items.length){
-      return videos.items[0]
-    } else {
-      return undefined;
-    }
-  },
   videos: function(){
     var videos = Session.get('videos');
     if (videos && videos.items && videos.items.length){
-      return videos.items.splice(1)
+      return videos.items
     } else {
       return [];
-    }
-  },
-  gridNumber: function(){
-    var videos = Session.get('videos');
-    if (videos && videos.items && videos.items.length){
-      if (videos.items.length < 4 && videos.items.length > 0){
-        return videos.items.length - 1;
-      } else {
-        return 3;
-      }
     }
   }
 });
